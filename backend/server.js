8aa3000bed976f22f2cdfa935d9a880494896667
Dhaval Zala <zaladhaval1818@gmail.com>
2026-04-24 17:48:44 +0530

    Deployment fix: Swapped to bcryptjs and updated CORS

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,11 @@ const io = new Server(server, {
 });
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Make Socket.io accessible to routes
